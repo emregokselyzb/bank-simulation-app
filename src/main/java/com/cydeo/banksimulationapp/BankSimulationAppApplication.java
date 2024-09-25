aa465b560e7939072ed3a4abdd5acbce16484bbf
emregokselyzb <emre.g.yzb@gmail.com>
2024-09-25 14:02:28 +0300

    validation started

--- src/main/java/com/cydeo/banksimulationapp/BankSimulationAppApplication.java
+++ src/main/java/com/cydeo/banksimulationapp/BankSimulationAppApplication.java
@@ -1,13 +1,30 @@
 package com.cydeo.banksimulationapp;
 
+import com.cydeo.enums.AccountType;
+import com.cydeo.model.Account;
+import com.cydeo.service.AccountService;
+import com.cydeo.service.TransactionService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @SpringBootApplication
 public class BankSimulationAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankSimulationAppApplication.class, args);
-	}
 
-}
+
+	ApplicationContext container=SpringApplication.run(BankSimulationAppApplication.class, args);
+
+	AccountService accountService=container.getBean(AccountService.class);
+	TransactionService transactionService=container.getBean(TransactionService.class);
+
+		Account sender=accountService.createNewAccount(BigDecimal.valueOf(70),new Date(), AccountType.SAVING,1L);
+		Account receiver=accountService.createNewAccount(BigDecimal.valueOf(50),new Date(),AccountType.CHECKING,2L);
+
+
+	}}

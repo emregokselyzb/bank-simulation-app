3ef19a1246b358e50d9aad25d30e36612dd0664a
emregokselyzb <emre.g.yzb@gmail.com>
2024-09-25 17:56:18 +0300

    validation started

---
+++ src/main/java/com/cydeo/dto/AccountDTO.java
@@ -0,0 +1,34 @@
+package com.cydeo.dto;
+
+import com.cydeo.enums.AccountType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountDTO {
+
+    private Long id;
+    @NotNull
+    @Positive
+    private BigDecimal balance;
+    @NotNull
+    private AccountType accountType;
+    private Date creationDate;
+    @NotNull
+    private Long userId;
+
+
+
+
+
+}

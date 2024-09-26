f919512afe8bdc52d2b9eba3f5213736ec5b72d0
emregokselyzb <emre.g.yzb@gmail.com>
2024-09-26 07:28:47 +0300

    AccountConverter added

--- src/main/java/com/cydeo/converter/AccountConverter.java
+++ src/main/java/com/cydeo/converter/AccountConverter.java
@@ -1,15 +1,27 @@
 package com.cydeo.converter;
 
 import com.cydeo.dto.AccountDTO;
+import com.cydeo.service.AccountService;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AccountConverter implements Converter<String, AccountDTO> {
-    @Override
-    public AccountDTO convert(String source) {
-        return null;
+
+    private final AccountService accountService;
+
+
+    public AccountConverter(AccountService accountService) {
+        this.accountService = accountService;
     }
 
+    @Override
+    public AccountDTO convert(String source){
+        if(source==null||source.equals("")){
+            return null;
+
+        }
+        return  accountService.retrieveByID(Long.parseLong(source));
+    }
 
 }

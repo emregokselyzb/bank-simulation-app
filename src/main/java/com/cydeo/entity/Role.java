3f847c6ac5c82ec7d7dcdc523aaa2e9a989542d2
emregokselyzb <emre.g.yzb@gmail.com>
2024-10-02 19:13:00 +0300

    implemented

--- src/main/java/com/cydeo/entity/Role.java
+++ src/main/java/com/cydeo/entity/Role.java
@@ -4,14 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
 @Table(name="roles")
 public class Role {
 

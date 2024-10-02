37f770334e50faa676ad26f6eabb8dad58d1608d
emregokselyzb <emre.g.yzb@gmail.com>
2024-10-02 12:56:02 +0300

    Security implemented

--- src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
@@ -1,16 +1,22 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.entity.User;
+import com.cydeo.entity.common.UserPrincipal;
+import com.cydeo.repository.UserRepository;
 import com.cydeo.service.SecurityService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.nio.file.attribute.UserPrincipal;
-
 @Service
 public class SecurityServiceImpl implements SecurityService {
 
     private final UserRepository userRepository;
+
+    public SecurityServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
